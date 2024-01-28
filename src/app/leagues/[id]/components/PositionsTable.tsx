e3774822a958c7dc8e86b5ee62c66293a007335b
Chris Hendrix <chendrix1123@gmail.com>
2024-01-27 19:29:03 -0500

    finish pos table

--- src/app/leagues/[id]/components/PositionsTable.tsx
+++ src/app/leagues/[id]/components/PositionsTable.tsx
@@ -1,24 +1,54 @@
 'use client'
 
 import { useDraft } from '@/hooks/draft'
+import { PlayerArgs } from '@/types'
 import { getPlayersByPosition } from '@/utils/draft'
+import Table, { TableColumn } from '@/components/Table'
 
 interface Props {
   draftId: string,
   teamId: string,
 }
 
+type PositionRecord = { position: string, players: PlayerArgs[] }
+
 const PositionsTable: React.FC<Props> = ({ draftId, teamId }) => {
   const { draft } = useDraft(draftId)
-  const draftPicks = draft?.draftPicks?.filter((dp) => dp.teamId === teamId)
-  const players = draftPicks.flatMap((dp) => (dp.player ? [dp.player] : []))
+  const team = draft?.draftTeams.find((dt) => dt.teamId === teamId)?.team
+  const draftPicks = draft?.draftPicks?.filter((dp) => dp.teamId === teamId) || []
+  const teamPlayers = draftPicks.flatMap((dp) => (dp.player ? [dp.player] : []))
+  const positionMap = getPlayersByPosition(teamPlayers)
 
-  console.log(getPlayersByPosition(players))
+  const data = Object.keys(positionMap).map((position) => ({
+    position,
+    players: positionMap[position]
+  }))
+
+  const columns: TableColumn<PositionRecord>[] = [
+    {
+      header: 'Pos',
+      value: ({ position }) => position
+    },
+    {
+      header: 'Picks',
+      renderedValue: ({ players }) => (
+        <>
+          {players.map((player) => (
+            <div key={player.id} className="badge badge-xs badge-primary" />
+          ))}
+        </>
+      )
+    }
+  ]
 
   return (
     <div>
-      <div>{draftId}</div>
-      <div>{teamId}</div>
+      {team && (
+        <label className="text-xs w-fit p-0.5" >
+          {team.name}
+        </label>
+      )}
+      <Table columns={columns} data={data} xs minHeight={'0px'} />
     </div>
   )
 }
