0d2cc27b57a23f3dcdcf53261fb5e21d4361553b
Chris Hendrix <chendrix1123@gmail.com>
2024-01-31 20:35:15 -0500

    change some colors

--- src/app/leagues/[id]/components/LeagueTab.tsx
+++ src/app/leagues/[id]/components/LeagueTab.tsx
@@ -13,7 +13,7 @@ const LeagueTab: React.FC<LeagueTabProps> = ({ leagueId }) => {
   if (isLoading) return
   return (
     <div className="flex flex-col w-full mt-8">
-      {url && <div className="bg-accent p-2 text-center text-accent-content shadow-md">
+      {url && <div className="bg-primary p-2 text-center text-primary-content shadow-md">
         {'Go to your '}
         <a className="link" href={url} target="_blank">
           season home
