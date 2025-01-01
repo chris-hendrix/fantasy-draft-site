39fce3253023d5ef666d1ee3cc44d99cbc9d4d08
Chris <chendrix1123@gmail.coms>
2025-01-01 15:59:00 -0500

    use NEXT_PUBLIC_VERCEL_ENV instead

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,9 @@ export const APP_URL = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : (process.env.NEXT_PUBLIC_URL || 'http://localhost:3000')
 
-export const VERCEL_ENV = process.env.VERCEL_ENV || 'local'
 
 export const API_URL = '/api'
 export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
 export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 export const SUPABASE_BUCKET = process.env.SUPABASE_BUCKET || NODE_ENV
+export const VERCEL_ENV = process.env.NEXT_PUBLIC_VERCEL_ENV || 'local'
