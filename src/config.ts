dca757c47fb49307125021440e099e3731f5bc75
Chris <chendrix1123@gmail.coms>
2025-01-01 16:00:54 -0500

    fix lint

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,6 @@ export const APP_URL = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : (process.env.NEXT_PUBLIC_URL || 'http://localhost:3000')
 
-
 export const API_URL = '/api'
 export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
 export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
